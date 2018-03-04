c066307e47aaaffd324f6690772621b4f77253d2
baur <bkozhaev@gmail.com>
2018-03-04 15:06:07 +0400

    implemented some changes to class basket

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,20 +1,21 @@
 class Basket
   attr_reader :basket, :money_amount
 
-  def initialize
+  def initialize(products)
     @basket = []
     @money_amount = 0
+    @products = products
+  end
+
+  def check_number?(user_choice)
+    user_choice.between?(1, @products.length)
   end
 
 
   def check_choice(products, user_choice)
-    size = products.length
-    if user_choice.between?(1, size)
+    if check_number?(user_choice)
       products[user_choice - 1].amount -= 1
       @basket << products[user_choice - 1]
-      puts "Вы выбрали: #{products[user_choice - 1]}"
-    else
-      puts "Вводите цифру в соответствии со списком предложенной продукции"
     end
   end
 
